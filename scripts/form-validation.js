a2447f49e0bd8c273e80a7825810db452f083d6d
Miguel <miguezaga2@hotmail.com>
2022-07-02 11:53:04 -0500

    added preventDefault

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -9,7 +9,8 @@ function addFormValidation() {
     let emailRegex =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    form.addEventListener("submit", function validate() {
+    form.addEventListener("submit", function validate(event) {
+        event.preventDefault();
         textField.classList.remove("text-field--success");
 
         if (input.value.match(emailRegex)) {
